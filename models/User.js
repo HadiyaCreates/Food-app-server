f3f0fb84ab1e0f9c3cc54b87e324cc9ef1f7c6f0
Hadiya <hadiyashaikh2006@gmail.com>
2025-07-24 15:09:43 +0530

    Clean code

--- models/User.js
+++ models/User.js
@@ -1,41 +1,4 @@
-// // // // backend/models/User.js
-// // // const mongoose = require("mongoose");
 
-// // // const userSchema = new mongoose.Schema({
-// // //   name: String,
-// // //   email: { type: String, unique: true },
-// // //   password: String,
-// // // });
-
-// // // module.exports = mongoose.model("User", userSchema);
-// // const mongoose = require("mongoose");
-
-// // const userSchema = new mongoose.Schema({
-// //   name: String,
-// //   email: { type: String, unique: true },
-// //   password: String,
-// //   profileImage: {
-// //     type: String,
-// //     default: "https://www.pngmart.com/files/23/Profile-PNG-Photo.png",
-// //   },
-// // });
-
-// // module.exports = mongoose.model("User", userSchema);
-
-// const mongoose = require("mongoose");
-
-// const userSchema = new mongoose.Schema({
-//   name: String,
-//   email: { type: String, unique: true },
-//   password: String,
-//   profileImage: {
-//     type: String,
-//     default: "https://www.pngmart.com/files/23/Profile-PNG-Photo.png",
-//   },
-// });
-
-// module.exports = mongoose.model("User", userSchema);
-// backend/models/User.js
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
